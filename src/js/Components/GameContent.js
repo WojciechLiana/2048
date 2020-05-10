5f6dfaac0b3970fe90c984474d0c93453e03ab78
Wojciech Liana <wojtek.liana@gmail.com>
2020-05-10 17:55:14 +0200

    correction Router bug for bundled files

--- src/js/Components/GameContent.js
+++ src/js/Components/GameContent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Link, Route} from "react-router-dom";
+import {HashRouter as Router, Link, Route} from "react-router-dom";
 import NewGame from "./NewGame";
 import About from "./About";
 import Rules from "./Rules";
