f1f6d8c48ed913971b008879527600bc622ae759
Wojciech Liana <wojtek.liana@gmail.com>
2020-05-10 13:12:47 +0200

    nawigation with W,S,A,D keys

--- src/js/logic/handleKeyboardArrows.js
+++ src/js/logic/handleKeyboardArrows.js
@@ -8,11 +8,19 @@ const handleKeyboardArrows = (keyPressed, board) => {
     switch (keyPressed) {
         case 'ArrowUp' :
             return moveUpHandler(board);
+        case 'KeyW' :
+            return moveUpHandler(board);
         case 'ArrowDown' :
+            return moveDownHandler(board);
+        case 'KeyS' :
             return moveDownHandler(board);
         case 'ArrowLeft' :
             return moveLeftHandler(board);
+        case 'KeyA' :
+            return moveLeftHandler(board);
         case 'ArrowRight' :
+            return moveRightHandler(board);
+        case 'KeyD' :
             return moveRightHandler(board);
         default :
             return board;
